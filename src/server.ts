843ca077b094ef47a0287cc9bb549bc846e1c14d
JoseLeon <21030026@itcelaya.edu.mx>
2025-06-01 22:01:22 -0600

    Crear y conectar base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import express from 'express'
 import router from './router'
 import db from './config/db'
+import colors from 'colors'
 
 //Conectar a bd
 async function connectDB(){
     try {
         await db.authenticate()
         db.sync()
-        console.log('Conexion EXITOSA')
+        console.log(colors.blue.bold('Conexion EXITOSA'))
 
     }catch(error){
         console.log(error)
-        console.log('ERROR al conectar a BD')
+        console.log(colors.red.bold('ERROR al conectar a BD'))
     }
 }
 connectDB()
