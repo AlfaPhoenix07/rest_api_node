c9340bf118167ec8a07139e491230e7557ba0027
JoseLeon <21030026@itcelaya.edu.mx>
2025-06-02 00:16:03 -0600

    Funcion agregar Producto

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,24 @@
 import {Router} from 'express'
+import { body } from 'express-validator'
+import { createProduct, getProducts } from './handlers/product'
+import { handleInputErrors } from './middleware'
 
 const router = Router()
 
 //Routing
-router.get('/', (req, res) => {
-    res.json('Desde GET')
-})
+router.get('/', getProducts)
 
-router.post('/', (req, res) => {
-    res.json('Desde POST')
-})
+router.post('/', 
+    //Validacion
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
+    body('price')
+        .isNumeric().withMessage('Valor no valido')
+        .notEmpty().withMessage('El precio no puede ir vacio')
+        .custom(value => value > 0).withMessage('El precio no puede ser menor a 0'),
+        handleInputErrors,
+    createProduct
+)
 
 router.put('/', (req, res) => {
     res.json('Desde PUT')
